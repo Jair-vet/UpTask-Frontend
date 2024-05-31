43b6cfd1c93477716eb784e98b3f72c091eb060c
Jair <jairaceves56@gmail.com>
2024-05-30 20:07:37 -0600

    Navegamos ente pagina crear producto y mis Proyectos

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
 
 export const CreateProjectView = () => {
   return (
-    <div>CreateProjectView</div>
+    <>
+       <h1 className="text-3xl font-black uppercase">Create Projects</h1>
+        <p className="text-xl font-light text-gray-500 mt-5">fill out form to create a project</p>
+
+        <nav className="my-5 ">
+            <Link
+                className="bg-purple-500 hover:bg-purple-600 px-10 py-3 rounded-lg text-white text-xl font-bold cursor-pointer transition-colors"
+                to={'/'}
+            >Back to Projects</Link>
+        </nav>
+    </>
   )
 }
