bf136ba3d18e5ce9bae8be396c9a74427ad53dad
Jair <jairaceves56@gmail.com>
2024-06-05 17:14:28 -0600

    Creamos la funcion para Obtener el Proyecto por ID y para Actualizar el Proyecto

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios"
-import { ProjectFormData, dashboardProjectSchema, } from "../types"
+import { Project, ProjectFormData, dashboardProjectSchema, editProjectSchema, } from "../types"
 import api from "@/lib/axios"
 
 // ! CREATE
@@ -14,6 +14,7 @@ export async function createProject(formData: ProjectFormData) {
     }
 }
 
+// ! Obtener un arreglo con los proyectos
 export async function getProjects() {
     try {
         const { data } = await api('/projects')
@@ -27,5 +28,37 @@ export async function getProjects() {
         if(isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         } 
+    }
+}
+
+// ! Obtener el Proyecto por ID
+export async function getProjectById(id: Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`)
+        const response = editProjectSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+
+type ProjectAPIType = {
+    formData: ProjectFormData
+    projectId: Project['_id']
+}
+
+export async function updateProject({formData, projectId} : ProjectAPIType ) {
+    try {
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
     }
 }
