217187626555370a65a7e02591bb36097e4e6096
Jair <jairaceves56@gmail.com>
2024-06-06 17:49:51 -0600

    Creamos el Formulario para las Tareas

---
+++ src/components/tasks/TaskForm.tsx
@@ -0,0 +1,53 @@
+import { FieldErrors, UseFormRegister } from "react-hook-form"
+import { TaskFormData } from "@/types/index";
+
+type TaskFormProps = {
+    errors: FieldErrors<TaskFormData>
+    register: UseFormRegister<TaskFormData>
+}
+
+export const TaskForm = ({errors, register} : TaskFormProps) => {
+  return (
+    <>
+      <div className="space-y-5 bg-white p-6 rounded-md">
+
+        <div className="flex flex-col gap-1">
+            <label
+                className="text-blue-500 text-sm uppercase font-bold"
+                htmlFor="name"
+            >Nombre de la tarea</label>
+            <input
+                id="name"
+                type="text"
+                placeholder="Nombre de la tarea"
+                className="input_formulario"
+                {...register("name", {
+                    required: "El nombre de la tarea es obligatorio",
+                })}
+            />
+            {/* {errors.name && (
+                <ErrorMessage>{errors.name.message}</ErrorMessage>
+            )} */}
+        </div>
+
+        <div className="flex flex-col gap-1">
+            <label
+                className="text-blue-500 text-sm uppercase font-bold"
+                htmlFor="description"
+            >Descripción de la tarea</label>
+            <textarea
+                id="description"
+                placeholder="Descripción de la tarea"
+                className="input_formulario"
+                {...register("description", {
+                    required: "La descripción de la tarea es obligatoria"
+                })}
+            />
+            {/* {errors.description && (
+                <ErrorMessage>{errors.description.message}</ErrorMessage>
+            )} */}
+        </div>
+      </div>
+    </>
+  )
+}
