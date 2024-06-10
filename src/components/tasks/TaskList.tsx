d825fc297d25456371719d61c8bd9ce93c0f027a
Jair <jairaceves56@gmail.com>
2024-06-10 13:58:53 -0600

    Creamos los encabezados de Colores en las Tareas

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,4 +1,4 @@
-import { Task, TaskProject } from "@/types/index"
+import {  TaskProject } from "@/types/index"
 import { TaskCard } from "./TaskCard"
 import { statusTranslations } from "@/locales/es"
 
@@ -26,6 +26,13 @@ const statusStyles: { [key: string]: string } = {
     underReview: 'border-t-amber-500',
     completed: 'border-t-emerald-500',
 }
+const borderStyles: { [key: string]: string } = {
+    pending: 'border-x-fuchsia-600',
+    onHold: 'border-x-red-600',
+    inProgress: 'border-x-blue-600',
+    underReview: 'border-x-amber-600',
+    completed: 'border-x-emerald-600',
+}
 
 export const TaskList = ({tasks} : TaskListProps) => {
 
@@ -36,20 +43,18 @@ export const TaskList = ({tasks} : TaskListProps) => {
         return { ...acc, [task.status]: currentGroup };
     }, initialStatusGroups);
 
-    console.log(groupedTasks);
-
 
   return (
     <>
         <h2 className="text-3xl text-white uppercase font-black my-10 pl-5">Tareas</h2>
 
-        <div className='w-full bg-slate-800 flex gap-5 pl-4 pr-4 overflow-x-scroll 2xl:overflow-auto pb-32'>
+        <div className='w-full pt-2 bg-slate-800 flex gap-5 pl-4 pr-4 overflow-x-scroll 2xl:overflow-auto pb-32'>
             {Object.entries(groupedTasks).map(([status, tasks]) => (
-                <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-                    <div className="border border-dashed border-y-slate-800 border-x-cyan-600">
-                        <h3
-                            className={`capitalize text-2xl font-bold text-center text-white bg-gray-600 p-3 border-t-8 ${statusStyles[status]} `}
-                        >{statusTranslations[status]}</h3>
+                <div key={status} className=' min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                    <h3
+                        className={`capitalize text-2xl font-bold text-center text-white bg-gray-600 p-3 border-t-8 ${statusStyles[status]} `}
+                    >{statusTranslations[status]}</h3>
+                    <div className={`mr-[1px] ml-[1px] border border-dashed border-y-slate-800  ${borderStyles[status]}`}>
 
                         <ul className='mt-5 space-y-5 p-3'>
                             {tasks.length === 0 ? (
