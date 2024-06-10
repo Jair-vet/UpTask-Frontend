23000b328c1ee5ba923cd7e181c4e445e766bdf1
Jair <jairaceves56@gmail.com>
2024-06-10 14:26:56 -0600

    Modificamos la Paleta de colores del Modal

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -40,7 +40,7 @@ export const TaskCard = ({ task }: TaskCardProps) => {
 
         <div className="flex shrink-0  gap-x-6">
             <Menu as="div" className="relative flex-none">
-                <Menu.Button className="-m-4 block p-2.5 text-white hover:text-gray-900">
+                <Menu.Button className="-m-4 block p-2.5 text-green-500 hover:text-white">
                     <span className="sr-only">opciones</span>
                     <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
                 </Menu.Button>
@@ -48,11 +48,11 @@ export const TaskCard = ({ task }: TaskCardProps) => {
                     enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75"
                     leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
                     <Menu.Items
-                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                        className={`absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-gray-700 py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none`}>
                         <Menu.Item>
                             <button
                                 type='button'
-                                className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                className='input_modal_botton text-green-500 hover:border-y-green-500'
                                 onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}
                             >
                                 Ver Tarea
@@ -63,7 +63,7 @@ export const TaskCard = ({ task }: TaskCardProps) => {
                             <Menu.Item>
                                 <button
                                     type='button'
-                                    className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                                    className='input_modal_botton text-blue-500 hover:border-y-blue-500'
                                     onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}
                                 >
                                     Editar Tarea
@@ -73,7 +73,7 @@ export const TaskCard = ({ task }: TaskCardProps) => {
                             <Menu.Item>
                                 <button
                                     type='button'
-                                    className='block px-3 py-1 text-sm leading-6 text-red-500'
+                                    className='input_modal_botton hover:border-y-red-500 text-red-500'
                                     // onClick={() => mutate({ projectId, taskId: task._id })}
                                 >
                                     Eliminar Tarea
