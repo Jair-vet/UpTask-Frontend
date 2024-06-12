ca5aebe313439608533c55d95b06c5da657a79c8
Jair <jairaceves56@gmail.com>
2024-06-12 16:30:07 -0600

    Creamos la vista para un Error 4040

--- src/views/404/NotFound.tsx
+++ src/views/404/NotFound.tsx
@@ -1,48 +1,13 @@
-import './style.css'
+import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
   return (
-    <div>
-        
-        <body className="bg-purple">
-        
-            <div className="stars">
-                <div className="custom-navbar">
-                    <div className="brand-logo">
-                        <img src="http://salehriaz.com/404Page/img/logo.svg" width="80px"/>
-                    </div>
-                    <div className="navbar-links">
-                        <ul>
-                            <li><a href="#" target="_blank">Home</a></li>
-                            <li><a href="#" target="_blank">About</a></li>
-                            <li><a href="#" target="_blank">Features</a></li>
-                            <li><a href="#" className="btn-request" target="_blank">Request A Demo</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="central-body">
-                    <img className="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px" />
-                    <a href="#" className="btn-go-home" target="_blank">GO BACK HOME</a>
-                </div>
-                <div className="objects">
-                    <img className="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px" />
-                    <div className="earth-moon">
-                        <img className="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px" />
-                        <img className="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px" />
-                    </div>
-                    <div className="box_astronaut">
-                        <img className="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px" />
-                    </div>
-                </div>
-                <div className="glowing_stars">
-                    <div className="star"></div>
-                    <div className="star"></div>
-                    <div className="star"></div>
-                    <div className="star"></div>
-                    <div className="star"></div>
-                </div>
-            </div>
-        </body>
-    </div>
+    <>
+        <h1 className="font-black text-center text-4xl text-white">Página No Encontrada</h1>
+        <p className="mt-10 text-center text-white">
+            Tal vez quieras volver a {' '}
+            <Link className=" text-fuchsia-500" to={'/'}>Proyectos</Link>
+        </p>
+    </>
   )
 }
